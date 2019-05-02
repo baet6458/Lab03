dd02ab1a730251412862de44e0657a09ac74e4ed
Elijsha Baetiong <baet6458@kettering.edu>
2019-05-02 00:51:50 -0400

    Adding display message

--- app/src/main/java/android/example/com/MainActivity.java
+++ app/src/main/java/android/example/com/MainActivity.java
@@ -34,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity*5);
+        String priceMessage= "Free";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -62,5 +63,13 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         quantity=quantity-1;
         display(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
